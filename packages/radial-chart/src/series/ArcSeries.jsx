58a4d34f0137e207da4316a328c2d7cc8c24dde7
Tom Arnoldussen <tom@route42.nl>
2019-02-13 11:42:35 +0100

    Added patch package changes

--- packages/radial-chart/src/series/ArcSeries.jsx
+++ packages/radial-chart/src/series/ArcSeries.jsx
@@ -9,31 +9,34 @@ import { singleHueScaleFactory } from '../util/fillScaleFactory';
 const grayScale = singleHueScaleFactory();
 
 const propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string,
-    }),
-  ).isRequired,
-  pieValue: PropTypes.func.isRequired, // (d) => pie value
-  pieSort: PropTypes.func,
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+            label: PropTypes.string,
+        })
+    ).isRequired,
+    pieValue: PropTypes.func.isRequired, // (d) => pie value
+    pieSort: PropTypes.func,
 
-  radius: PropTypes.number, // likely passed by parent
-  innerRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]), // (radius) => num
-  outerRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]), // (radius) => num
-  labelRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]), // (radius) => num
-  labelComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
-  label: PropTypes.func, // (arc) => node
+    radius: PropTypes.number, // likely passed by parent
+    innerRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]), // (radius) => num
+    outerRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]), // (radius) => num
+    labelRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]), // (radius) => num
+    labelComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.element]),
+    label: PropTypes.func, // (arc) => node
 
-  stroke: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-  strokeWidth: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
-  fill: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-  fillOpacity: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
+    stroke: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    strokeWidth: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
+    fill: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    fillOpacity: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
 
-  padAngle: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
-  padRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
-  cornerRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
-  onMouseMove: PropTypes.func,
-  onMouseLeave: PropTypes.func,
+    padAngle: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
+    padRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
+    cornerRadius: PropTypes.oneOfType([PropTypes.func, PropTypes.number]),
+    onMouseMove: PropTypes.func,
+    onMouseLeave: PropTypes.func,
+    onClick: PropTypes.func,
+    centerValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    centerValueLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 const INNER_RADIUS_FRACTION = 0.5;
@@ -42,103 +45,154 @@ const LABEL_RADIUS_FRACTION = 0.75;
 const MIN_ANGLE_FOR_LABEL = 0.1;
 
 const defaultProps = {
-  radius: 300,
-  pieSort: null,
-  innerRadius: radius => radius * INNER_RADIUS_FRACTION,
-  outerRadius: radius => radius * OUTER_RADIUS_FRACTION,
-  labelRadius: radius => radius * LABEL_RADIUS_FRACTION,
-  cornerRadius: 3,
-  fill: arc => (arc.data && arc.data.label ? grayScale(arc.data.label) : '#dddddd'),
-  fillOpacity: 1,
-  stroke: '#ffffff',
-  strokeWidth: 1,
-  labelComponent: <ArcLabel />,
-  label: null,
-  padAngle: null,
-  padRadius: null,
-  onMouseMove: null,
-  onMouseLeave: null,
+    radius: 300,
+    pieSort: null,
+    innerRadius: (radius) => radius * INNER_RADIUS_FRACTION,
+    outerRadius: (radius) => radius * OUTER_RADIUS_FRACTION,
+    labelRadius: (radius) => radius * LABEL_RADIUS_FRACTION,
+    cornerRadius: 3,
+    fill: (arc) =>
+        arc.data && arc.data.label ? grayScale(arc.data.label) : '#dddddd',
+    fillOpacity: 1,
+    stroke: '#ffffff',
+    strokeWidth: 1,
+    labelComponent: <ArcLabel />,
+    label: null,
+    padAngle: null,
+    padRadius: null,
+    onMouseMove: null,
+    onMouseLeave: null,
+    onClick: null,
+    centerValue: null,
+    centerValueLabel: null,
 };
 
-export default function ArcSeries({
-  data,
-  radius,
-  pieValue,
-  pieSort,
-  innerRadius,
-  outerRadius,
-  labelRadius,
-  fill,
-  fillOpacity,
-  cornerRadius,
-  padAngle,
-  padRadius,
-  stroke,
-  strokeWidth,
-  label,
-  labelComponent,
-  onMouseMove,
-  onMouseLeave,
-  ...restProps
-}) {
-  return (
-    <g>
-      <Arc
-        data={data}
-        pieValue={pieValue}
-        pieSort={pieSort}
-        outerRadius={callOrValue(outerRadius, radius)}
-        innerRadius={callOrValue(innerRadius, radius)}
-        fill={fill}
-        fillOpacity={fillOpacity}
-        stroke={stroke}
-        strokeWidth={strokeWidth}
-        cornerRadius={cornerRadius}
-        padAngle={padAngle}
-        padRadius={padRadius}
-        onMouseMove={
-          onMouseLeave &&
-          (datum => event => {
-            const fraction = Math.abs(datum.startAngle - datum.endAngle) / (2 * Math.PI);
-            onMouseMove({
-              event,
-              data,
-              datum: datum.data,
-              fraction,
-            });
-          })
-        }
-        onMouseLeave={
-          onMouseLeave &&
-          (() => () => {
-            onMouseLeave();
-          })
-        }
-        {...restProps}
-        centroid={null}
-      />
-      {label && labelComponent && (
-        <Arc
-          data={data}
-          pieValue={pieValue}
-          pieSort={pieSort}
-          outerRadius={callOrValue(labelRadius, radius)}
-          innerRadius={callOrValue(labelRadius, radius)}
-          fill="none"
-          fillOpacity={0}
-          stroke="none"
-          strokeWidth={0}
-          centroid={(centroid, arc) => {
-            const [x, y] = centroid;
-            const labelElement = label(arc);
-            if (arc.endAngle - arc.startAngle < MIN_ANGLE_FOR_LABEL || !labelElement) return null;
+export default class ArcSeries extends React.PureComponent {
+    render() {
+        const {
+            data,
+            radius,
+            pieValue,
+            pieSort,
+            innerRadius,
+            outerRadius,
+            labelRadius,
+            fill,
+            fillOpacity,
+            cornerRadius,
+            padAngle,
+            padRadius,
+            stroke,
+            strokeWidth,
+            label,
+            labelComponent,
+            onMouseMove,
+            onMouseLeave,
+            onClick,
+            centerValue,
+            centerValueLabel,
+            ...restProps,
+        } = this.props;
+        return (
+            <g>
+                <Arc
+                    data={data}
+                    pieValue={pieValue}
+                    pieSort={pieSort}
+                    outerRadius={callOrValue(outerRadius, radius)}
+                    innerRadius={callOrValue(innerRadius, radius)}
+                    fill={fill}
+                    fillOpacity={fillOpacity}
+                    stroke={stroke}
+                    strokeWidth={strokeWidth}
+                    cornerRadius={cornerRadius}
+                    padAngle={padAngle}
+                    padRadius={padRadius}
+                    onMouseMove={
+                        onMouseLeave &&
+                        ((datum) => (event) => {
+                            const fraction =
+                                Math.abs(datum.startAngle - datum.endAngle) / (2 * Math.PI);
+                            onMouseMove({
+                                event,
+                                data,
+                                datum: datum.data,
+                                fraction,
+                            });
+                        })
+                    }
+                    onMouseLeave={
+                        onMouseLeave &&
+                        (() => () => {
+                            onMouseLeave();
+                        })
+                    }
+                    onClick={
+                        onClick &&
+                        (d => event => {
+                            const { key: seriesKey, data: datum } = d;
+                            onClick({
+                                event,
+                                data,
+                                datum,
+                                seriesKey,
+                            });
+                        })
+                    }
+                    {...restProps}
+                    centroid={null}
+                />
+                { centerValue && centerValueLabel &&
+                <g key={`totalValue`}>
+                    <text
+                        fill="black"
+                        textAnchor="middle"
+                        fontSize={22}
+                    >
+                        {centerValue}
+                    </text>
+                    <text
+                        fill="black"
+                        textAnchor="middle"
+                        fontSize={14}
+                        transform="translate(0,20)"
+                    >
+                        {centerValueLabel}
+                    </text>
+                </g>
+                }
+                {label &&
+                labelComponent && (
+                    <Arc
+                        data={data}
+                        pieValue={pieValue}
+                        pieSort={pieSort}
+                        outerRadius={callOrValue(labelRadius, radius)}
+                        innerRadius={callOrValue(labelRadius, radius)}
+                        fill="none"
+                        fillOpacity={0}
+                        stroke="none"
+                        strokeWidth={0}
+                        centroid={(centroid, arc) => {
+                            const [x, y] = centroid;
+                            const labelElement = label(arc);
+                            if (
+                                arc.endAngle - arc.startAngle < MIN_ANGLE_FOR_LABEL ||
+                                !labelElement
+                            )
+                                return null;
 
-            return React.cloneElement(labelComponent, { x, y, arc }, labelElement);
-          }}
-        />
-      )}
-    </g>
-  );
+                            return React.cloneElement(
+                                labelComponent,
+                                {x, y, arc},
+                                labelElement
+                            );
+                        }}
+                    />
+                )}
+            </g>
+        );
+    }
 }
 
 ArcSeries.propTypes = propTypes;
